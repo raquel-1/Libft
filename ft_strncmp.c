2ca1c5db2fe4a70e0d5a40940d1826250f03cf1f
raquel-1 <rocamartinezraquel@gmail.com>
2026-01-13 18:56:35 +0100

    ft_strchr.c ft_strncmp.c ft_strnstr.c ft_strrchr.c

---
+++ ft_strncmp.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 17:17:08 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/01/13 17:29:54 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+max n compare
+0 → strings are equal
+< 0 → str1 is less than str2
+> 0 → str1 is greater than str2
+compare ascii values -> s1 - s2*/
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n && s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}*/
---
+++ ft_strnstr.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 13:57:21 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/01/13 17:10:07 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(char *haystack, char *needle, unsigned int len)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return (haystack);
+	while (haystack[i] && (i < len))
+	{
+		j = 0;
+		while ((haystack[i + j] == needle[j]) && (i + j < len) && (needle[j]))
+			j++;
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
+/*
+(needle[j])  ==>  needle[j] != '\0' // no hemos llegado al final de needle
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (to_find[0] == '\0') // devuevo str si toFind es vacio
+		return (str);
+	while (str[i])
+	{
+		j = 0;
+		while (str[i + j] == to_find[j])
+		{
+			j++;
+			if (to_find[j] == '\0')
+				return (&str[i]);  //Devuelve puntero al inicio del toFind
+		}
+		i++;
+	}
+	return (NULL);
+}
+*/
