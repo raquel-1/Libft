8ce4afb9b890147646020a5492e03ee9d90c0ffb
raqroca- <raqroca-@student.42madrid.com>
2026-01-14 10:33:39 +0100

    rest.h

--- libft.h
+++ libft.h
@@ -6,13 +6,18 @@
 /*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 21:07:26 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/01/13 21:39:37 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/01/14 10:32:05 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
 int				ft_strlen(char *str);
 char			*ft_strchr(char *s, int c);
 char			*ft_strrchr(char *s, int c);
