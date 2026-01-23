cd8f30d2d72906d27fbc0cf17206d5bd74bd7b55
raqroca- <raqroca-@student.42madrid.com>
2026-01-23 13:11:56 +0100

    ft_strtrim ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 13:04:29 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/01/23 12:46:32 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/01/23 13:11:26 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 #include "libft.h"
 
 static int	is_in_set(char c, char const *set);
+static char	*ft_copy_trim(char const *s1, size_t start, size_t len);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	end;
-	char	*result;
 	size_t	len;
-	size_t	i;
 
 	if (!s1 || !set)
 		return (NULL);
@@ -38,11 +37,19 @@ char	*ft_strtrim(char const *s1, char const *set)
 		len = 0;
 	else
 		len = end - start + 1;
+	return (ft_copy_trim(s1, start, len));
+}
+
+static char	*ft_copy_trim(char const *s1, size_t start, size_t len)
+{
+	char	*result;
+	size_t	i;
+
 	result = malloc(len + 1);
 	if (!result)
 		return (NULL);
 	i = 0;
-	while (i < (len))
+	while (i < len)
 	{
 		result[i] = s1[start + i];
 		i++;
@@ -61,3 +68,28 @@ static int	is_in_set(char c, char const *set)
 	}
 	return (0);
 }
+/*
+int	main(void)
+{
+	char	*test;
+
+	test = ft_strtrim("   hello world   ", " ");
+	printf("Test 1: '%s'\n", test);//hello world
+	free(test);
+	test = ft_strtrim("xxxhello worldxxx", "x");
+	printf("Test 2: '%s'\n", test);//hello world
+    free(test);
+	test = ft_strtrim("   ", " ");
+	printf("Test 3: '%s'\n", test);//empty
+	free(test);
+	test = ft_strtrim("hello", " ");
+	printf("Test 4: '%s'\n", test);//hello
+	free(test);
+	test = ft_strtrim("ababcabaXYZcababa", "ab");
+	printf("Test 5: '%s'\n", test);//cXYZc
+	free(test);
+	test = ft_strtrim("", "x");
+	printf("Test 6: '%s'\n", test);//empty
+	free(test);
+	return (0);
+}*/
