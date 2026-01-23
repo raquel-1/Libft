ad48f9cb315c2316578b00fcf5c9cf01c73bd6d3
raqroca- <raqroca-@student.42madrid.com>
2026-01-23 16:15:49 +0100

    ft_substr.c paco

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 11:22:25 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/01/19 16:05:38 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/01/23 16:15:04 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
+	size_t	s_len;
 	size_t	i;
 
 	if (!s)
 		return (NULL);
-	if (start >= (unsigned int)ft_strlen(s))
-		len = 0;
-	if (len > (size_t)ft_strlen(s + start))
-		len = ft_strlen(s + start);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_calloc(1, sizeof(char)));
+	if (len > s_len - start)
+		len = s_len - start;
 	sub = (char *)malloc(len + 1);
 	if (!sub)
 		return (NULL);
