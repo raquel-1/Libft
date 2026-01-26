109ca8658783398d7a4ad03d69fadc9e112f72a1
raquel-1 <rocamartinezraquel@gmail.com>
2026-01-26 19:03:34 +0100

    lstadd_front

---
+++ ft_lstadd_front.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 18:43:46 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/01/26 19:03:13 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
