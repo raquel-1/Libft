c2437137e752b84b2d4165504836aa364c230e85
raqroca- <raqroca-@student.42madrid.com>
2026-01-27 16:27:10 +0100

    ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 15:01:22 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/01/27 16:26:46 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+	t_list	*p_next;
+
+	if (!*lst)
+	{
+		return ;
+	}
+	temp = *lst;
+	while (temp)
+	{
+		p_next = temp->next;
+		del(temp->content);
+		free(temp);
+		temp = p_next;
+	}
+	*lst = NULL;
+}
