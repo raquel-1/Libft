9211e031f717169498bc0802b60236ecc6300c2d
raqroca- <raqroca-@student.42madrid.com>
2026-01-27 13:02:57 +0100

    ft_lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 11:44:04 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/01/27 13:02:17 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*end;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	end = *lst;
+	while (end->next != NULL)
+		end = end->next;
+	end->next = new;
+}
