a595a32c1f2ac361849644d2c530430ec9959ee8
raqroca- <raqroca-@student.42madrid.com>
2026-01-27 16:43:02 +0100

    ft_lsttiter

---
+++ ft_lstiter.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 16:27:59 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/01/27 16:42:33 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
