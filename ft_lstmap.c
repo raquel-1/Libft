6b94b4df3ddb940c5395f270cce113fa67c62576
raquel-1 <rocamartinezraquel@gmail.com>
2026-01-28 17:38:37 +0100

    lstmap, part 3 in Makefile and .h

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,13 +6,31 @@
 /*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 16:43:41 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/01/27 17:07:23 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/01/28 17:27:23 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{	
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*result_lst;
+	t_list	*node;
+
+	result_lst = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	while (lst)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&result_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&result_lst, node);
+		lst = lst->next;
+	}
+	return (result_lst);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 21:07:26 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/01/26 18:07:21 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/01/28 17:08:04 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,14 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 /*
 part 3*/
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
