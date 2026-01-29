5501d7282f2d7d59675847b5c3dae6732ccd4377
raquel-1 <rocamartinezraquel@gmail.com>
2026-01-29 21:40:21 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
+/*   By: raromar <raromar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 16:13:04 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/01/29 11:09:36 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/01/29 21:38:37 by raromar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,48 +22,3 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_bzero(tmp, size * nmemb);
 	return (tmp);
 }
-/*
-int	main(void)
-{
-	size_t n = 5;
-	size_t i;
-	int *arr1;
-	int *arr2;
-
-	arr1 = (int *)ft_calloc(n, sizeof(int));
-	if (!arr1)
-	{
-		printf("ERROR ft_calloc\n");
-		return (1);
-	}
-
-	printf("ValUES ft_calloc: ");
-	i = 0;
-	while (i < n)
-	{
-		printf("%d ", arr1[i]);
-		i++;
-	}
-	printf("\n");
-
-	arr2 = (int *)calloc(n, sizeof(int));
-	if (!arr2)
-	{
-		printf("ERROR calloc\n");
-		free(arr1);
-		return (1);
-	}
-
-	printf("VALUES  CALLOC: ");
-	i = 0;
-	while (i < n)
-	{
-		printf("%d ", arr2[i]);
-		i++;
-	}
-	printf("\n");
-
-	free(arr1);
-	free(arr2);
-	return (0);
-}*/
